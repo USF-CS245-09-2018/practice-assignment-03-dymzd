2ccb209bd4ea561788c5d51d8075d857c677bbaf
Daiya Masuda <dmasuda@dons.usfca.edu>
2018-09-07 11:42:58 -0700

    done

--- Practice03Test.java
+++ Practice03Test.java
@@ -54,11 +54,34 @@ public class Practice03Test {
 
 	public int find_min_iterative () {
 		// TODO: Fill in this iterative function.
+		int min = 0;
+		for (int i = 0; i < arr.length; i++){
+			if(arr[i] < arr[min]){
+				min = i; 
+			}
+		}
+		return min;
 	}
 
 
 	public int find_min_recursive () {
 		// TODO: Fill in this recursive function.
+		int min = 0;
+		int cur = 0;
+
+		int result = find_min_recursive(arr,min,cur);
+		return result;
+	}
+	public int find_min_recursive (double[] arr, int min, int cur) {
+		// TODO: Fill in this recursive function.
+		if (cur >= arr.length-1){
+			return min;
+		} else {
+				if (arr[cur] < arr[min]){
+					return find_min_recursive(arr,cur,cur+1);
+				}
+				return find_min_recursive(arr, min, cur+1);
+		}
 	}
 
 
